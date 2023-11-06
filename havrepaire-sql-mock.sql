166e374fe68facdab32470227988b315a71c7c22
Kim Robert <kimrobert.nb@gmail.com>
2023-11-06 17:52:58 +0100

    t002 feat: add data insert with additional data for tests - add create and update time fields in each table

--- havrepaire-sql-mock.sql
+++ havrepaire-sql-mock.sql
@@ -12,52 +12,62 @@ USE `havrepairedb`;
 -- Add tables --
 CREATE TABLE IF NOT EXISTS `articles` (
   PRIMARY KEY (articleId),
-  `articleId` BINARY(16) NOT NULL UNIQUE,
+  `articleId` VARCHAR(36) NOT NULL UNIQUE,
   `title_Fr` VARCHAR(120) NOT NULL UNIQUE,
   `title_En` VARCHAR(120),
-  `text_En` VARCHAR(1200),
-  `text_Fr` VARCHAR(1200) NOT NULL,
-  `categories` SET ('short', 'long', 'news', 'fantasy', 'science_fiction'),
-  `state` ENUM ('draft', 'published', 'archived') NOT NULL DEFAULT 'draft'
+  `text_Fr` VARCHAR(1200),
+  `text_En` VARCHAR(1200) NOT NULL,
+  `category` ENUM ('short', 'long', 'news', 'fantasy', 'science_fiction') NOT NULL DEFAULT 'news',
+  `state` ENUM ('draft', 'published', 'archived') NOT NULL DEFAULT 'draft',
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (commentId),
-  `commentId` BINARY(16) NOT NULL UNIQUE,
+  `commentId` VARCHAR(36) NOT NULL UNIQUE,
   `text` VARCHAR(500) NOT NULL,
-  `languages` SET ('fr', 'en') DEFAULT 'fr',
-  `userId` BINARY(16) NOT NULL,
-  `articleId` BINARY(16) NOT NULL
+  `language` ENUM ('fr', 'en') DEFAULT 'fr',
+  `userId` VARCHAR(36) NOT NULL,
+  `articleId` VARCHAR(36) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `illustrations` (
   PRIMARY KEY (illustrationId),
-  `illustrationId` BINARY(16) NOT NULL UNIQUE,
+  `illustrationId` VARCHAR(36) NOT NULL UNIQUE,
   `filename` VARCHAR(80) NOT NULL UNIQUE,
   `filepath` VARCHAR(256) NOT NULL UNIQUE,
-  `articleId` BINARY(16) NOT NULL
+  `articleId` VARCHAR(36) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `likes` (
   PRIMARY KEY (likeId),
-  `likeId` BINARY(16) NOT NULL UNIQUE,
-  `userId` BINARY(16) NOT NULL,
-  `articleId` BINARY(16) NOT NULL
+  `likeId` VARCHAR(36) NOT NULL UNIQUE,
+  `userId` VARCHAR(36) NOT NULL,
+  `articleId` VARCHAR(36) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (userId),
-  `userId` BINARY(16) NOT NULL UNIQUE,
+  `userId` VARCHAR(36) NOT NULL UNIQUE,
   `username` VARCHAR(80) NOT NULL UNIQUE,
   `email` VARCHAR(120) NOT NULL UNIQUE,
   `hash` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `gender` ENUM ('m', 'f', 'n') NOT NULL DEFAULT 'n',
   `pronouns` VARCHAR(80),
-  `role` ENUM ('admin', 'viewer', 'ghost') NOT NULL DEFAULT 'viewer'
+  `role` ENUM ('admin', 'viewer', 'ghost') NOT NULL DEFAULT 'viewer',
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
 )
 ENGINE = InnoDB;
 
@@ -76,4 +86,47 @@ ALTER TABLE `illustrations`
 
 
 -- End transaction --
+COMMIT;
+
+
+-- Add data --
+
+-- (users table) --
+INSERT INTO `users` VALUES
+(UUID(), 'admin', 'admin@fake.net', 'FakePass@44!', DEFAULT, 'iel', 'admin', DEFAULT, DEFAULT),
+('54c4c1d9-7cb4-11ee-a619-9828a647f095', 'titi', 'titi@fake.net', 'TitiPass@44!', DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'toto', 'toto@fake.net', 'TotoPass@44!', 'm', DEFAULT, DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'tonton', 'tonton@fake.net', 'TontonPass@44!', 'm', 'il', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'tata', 'tata@fake.net', 'TataPass@44!', 'f', 'elle /ael', DEFAULT, DEFAULT, DEFAULT);
+
+-- (articles) --
+INSERT INTO `articles` VALUES
+('41d6022e-7cb4-11ee-a619-9828a647f095', 'Premier Article', DEFAULT, 'Ceci est le texte de mon tout premier article', DEFAULT, 'news', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article traduit', 'Translated Article', 'Cet article est traduit.', 'This article is translated.', 'news', DEFAULT, DEFAULT, DEFAULT),
+('54c7578b-7cb4-11ee-a619-9828a647f095', 'Article formidable', 'Awesome Article', 'Voici mon super article !', 'Here is my great article !', ('fantasy'), 'published', DEFAULT, DEFAULT),
+(UUID(), 'Article test 1', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'news', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 2', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'news', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 3', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'fantasy', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 4', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'science_fiction', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 5', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'news', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 6', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'science_fiction', DEFAULT, DEFAULT, DEFAULT),
+(UUID(), 'Article test 7', DEFAULT, 'Ceci est le texte de test', DEFAULT, 'fantasy', DEFAULT, DEFAULT, DEFAULT);
+
+-- (illustrations) --
+INSERT INTO `illustrations` VALUES
+(UUID(), 'illus_article_1', '/public/images/illus_article_1.jpeg', '41d6022e-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT),
+(UUID(), 'illus_super', '/public/images/super_illus.webp', '54c7578b-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT);
+
+-- (likes) --
+INSERT INTO `likes` VALUES
+(UUID(), '54c4c1d9-7cb4-11ee-a619-9828a647f095', '41d6022e-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT),
+(UUID(), '54c4c1d9-7cb4-11ee-a619-9828a647f095', '54c7578b-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT);
+
+-- (comments) --
+INSERT INTO `comments` VALUES
+(UUID(), 'I love this text !', 'en', '54c4c1d9-7cb4-11ee-a619-9828a647f095', '54c7578b-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT),
+(UUID(), 'commentaire test 1', 'fr', '54c4c1d9-7cb4-11ee-a619-9828a647f095', '41d6022e-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT),
+(UUID(), 'commentaire test 2', 'fr', '54c4c1d9-7cb4-11ee-a619-9828a647f095', '41d6022e-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT),
+(UUID(), 'commentaire test 3', DEFAULT, '54c4c1d9-7cb4-11ee-a619-9828a647f095', '41d6022e-7cb4-11ee-a619-9828a647f095', DEFAULT, DEFAULT);
+
 COMMIT;
